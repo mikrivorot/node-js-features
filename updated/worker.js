93f2619a449ffcdeb320149272c387e747983d4d
mikrivorot <m.i.krivorot@gmail.com>
2018-07-03 14:58:13 +0300

    Separate threads into two files, add event listener

---
+++ updated/worker.js
@@ -0,0 +1,22 @@
+const {  parentPort } = require('worker_threads');
+
+console.log('started');
+
+function random(min, max) {
+    return Math.random() * (max - min) + min
+}
+
+const sorter = require("./list-sorter");
+
+const start = Date.now()
+let bigList = Array(1000000).fill().map( (_) => random(1,10000))
+
+/**
+ //вот как получить сообщение из главного потока:
+ parentPort.on('message', (msg) => {
+    console.log("Main thread finished on: ", (msg.timeDiff / 1000), " seconds...");
+})
+ */
+
+sorter.sort(bigList);
+parentPort.postMessage({ val: sorter.firstValue, timeDiff: Date.now() - start});
